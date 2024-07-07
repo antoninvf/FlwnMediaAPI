f9585022d2880df0ccca901fab00bb93436b5da8
Antonín <flowythe@gmail.com>
2024-07-07 23:28:33 +0200

    removed unused

--- Program.cs
+++ Program.cs
@@ -15,16 +15,10 @@ builder.Services.AddCors(options =>
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
 app.UseCors();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
